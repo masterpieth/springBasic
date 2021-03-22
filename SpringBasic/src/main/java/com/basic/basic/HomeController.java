cc944092b29d81f39b943cabf0515d4284f5ba64
nykim <kny0339@gmail.com>
2021-03-22 16:02:46 +0900

    2021-03-22 클라이언트 ip 가져오기

--- SpringBasic/src/main/java/com/basic/basic/HomeController.java
+++ SpringBasic/src/main/java/com/basic/basic/HomeController.java
@@ -16,7 +16,6 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
-		
 		return "/home";
 	}
 	
--- SpringBasic/src/main/java/com/basic/basic/controller/MainController.java
+++ SpringBasic/src/main/java/com/basic/basic/controller/MainController.java
@@ -1,14 +1,21 @@
 package com.basic.basic.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
 	@RequestMapping(value = "/login/loginForm", method = RequestMethod.GET)
-	public String loginForm() {
+	public String loginForm(HttpServletRequest request) {
+		logger.debug("request debug");
 		return "/login/loginForm";
 	}
 	@RequestMapping(value = "/admin/adminHome", method = RequestMethod.GET)
